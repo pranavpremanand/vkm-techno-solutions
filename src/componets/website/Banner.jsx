a2a19cc9eb07878f949a1d8ea2c1e933bc86c16f
Pranav <mpranavprem@gmail.com>
2024-11-05 21:54:21 +0530

    styles changes

--- src/componets/website/Banner.jsx
+++ src/componets/website/Banner.jsx
@@ -3,9 +3,9 @@ import { defaultBanner } from "../../constant";
 
 const Banner = ({ page }) => {
   return (
-    <div data-aos="fade-down" className="h-[40vh] sm:h-[45vh] md:h-[60vh] relative">
+    <div data-aos="fade-down" className="h-[30vh] sm:h-[45vh] md:h-[60vh] relative">
       <img src={defaultBanner} className="object-cover h-full w-full" alt="" />
-      <h2 className="font-raleway text-[2.15rem] md:text-7xl font-bold leading-tight text-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+      <h2 className="font-raleway text-[2.18rem] md:text-7xl font-bold leading-tight text-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
         {page}
       </h2>
     </div>
